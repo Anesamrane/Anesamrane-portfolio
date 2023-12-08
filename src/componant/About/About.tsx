5e9d087f56095d57444f2102e511012cf7b08070
Anesamrane <121605342+Anesamrane@users.noreply.github.com>
2023-12-08 08:26:26 +0100

    Add files via upload

--- src/componant/About/About.tsx
+++ src/componant/About/About.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 import Image from 'next/image'
 import anespic from '../../../public/photo1701791600.jpeg'
@@ -8,13 +9,10 @@ function About() {
     <div className="about" id='About'>
         <h4> About me <hr /></h4>
         <div className="detail">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Fugit iure unde earum quas est tempora fuga voluptate expedita
-           aliquid totam, ex enim at, ipsam illo, asperiores sed quasi natus! Natus. 
-           Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, amet dolores! Optio,
-            quis. Quod eaque, iste possimus saepe, 
-          cupiditate aut odio consequatur modi iure recusandae debitis eum officiis molestiae veritatis!
-
+        <p>i'm Anes amrane a full stack divelopper i have more then 3 year experience and 
+          i build more then 10 website with different technologies and with different features
+          i born in 5 january 2002 in algeria i study computer science in saad dahleb in blidai success
+          in my bacalouria with 13:50 i have been building and deploying website all my hole life 
 
           <h4>My skills in dev web</h4>
           <div className="skill">
